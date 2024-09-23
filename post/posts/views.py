88a654937185d4e99d72ed14879559457b788b72
tkddk0108 <cometokr1@naver.com>
2024-09-24 05:31:40 +0900

    Post-create

--- post/posts/views.py
+++ post/posts/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from .models import Post
+from django.shortcuts import render, redirect
+from .forms import PostForm
+
+
+
 
 # 게시글 목록
 def post_list(request):
@@ -10,3 +15,14 @@ def post_list(request):
 def post_detail(request, pk):
     post = Post.objects.get(pk=pk)
     return render(request, 'posts/post_detail.html', {'post': post})
+
+# 게시글 생성하는 뷰
+def post_create(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('post_list')  # 게시글 목록으로 리다이렉트
+    else:
+        form = PostForm()
+    return render(request, 'posts/post_form.html', {'form': form})
