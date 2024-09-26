3e23b4131bb3dcf59d22b4b1b7ee494d0d66469b
chrisheo <jftj97@gmail.com>
2024-09-26 11:59:00 +0900

    [fix] import include at urls.py

--- voting_system/voting_system/urls.py
+++ voting_system/voting_system/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from voting_system import views  # 방금 만든 뷰를 가져옵니다
 
 urlpatterns = [
